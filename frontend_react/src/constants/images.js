51d38713c6a25e797257404c6104337a174ed16e
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-12-03 01:20:08 -0400

    setup navbar

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,5 +1,7 @@
 import logo from '../assets/Richard Leung woo Gabriel.png';
+import logo2 from '../assets/Richard_Leung_woo_Gabriel-removebg-preview.png';
+import hero from '../assets/bg-1.png';
 
 export default {
-  logo,
+  logo, logo2, hero,
 };
--- frontend_react/tailwind.config.js
+++ frontend_react/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#5D3BEE',
+        secondary: '#51586A',
+        header: '#FBFAFF',
+      },
+
+    },
   },
   plugins: [],
 };
