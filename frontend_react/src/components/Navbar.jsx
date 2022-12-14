21ab205ec5627cdf6eab4af4d2abeefeecc64066
Tidbits JS <tidbitsjs@gmail.com>
2022-12-14 20:11:21 +0530

    navbar fix

--- frontend_react/src/components/Navbar.jsx
+++ frontend_react/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar({ toggleTheme, theme }) {
       >
         <img src={logo} alt="logo" className="flex-1 object-cover max-w-[70px] max-h-[70px] shadow-primary shadow-sm rounded-full" />
       </a>
-      <ul className="links hidden md:flex flex-1 justify-center items-center list-none text-secondary text-base">
+      <ul className="links hidden md:flex flex-1 justify-end items-center list-none text-secondary text-base">
         {['HOME', 'ABOUT', 'PROJECTS', 'CONTACT', <Button btnName="RESUME" />, theme === 'light' ? <BsFillMoonStarsFill onClick={toggleTheme} size={20} /> : <BsSunFill onClick={toggleTheme} size={20} color="yellow" />].map((item) => (
           <li key={`link - ${item}`} className="flex flex-col mx-4 cursor-pointer justify-center items-center ">
             <motion.a
@@ -37,7 +37,7 @@ function Navbar({ toggleTheme, theme }) {
 
       <div className="appBarMenu flex md:hidden w-[39px] h-[35px] justify-center items-center relative rounded-full bg-primary">
         <HiMenu className="text-white w-[70%] h-[70%] " onClick={() => setToggle(true)} />
-        {toggle && <Sidebar setToggle={setToggle} toggle={toggle} toggleTheme={toggleTheme} theme={theme} />}
+        <Sidebar setToggle={setToggle} toggle={toggle} toggleTheme={toggleTheme} theme={theme} />
       </div>
     </nav>
   );
