99ee1fb9837dfa2fb7e184a91e5cf4ff7d724fec
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-06-26 20:05:54 -0400

    update sidebar link to resume and removed .env file

--- frontend_react/src/components/Sidebar.jsx
+++ frontend_react/src/components/Sidebar.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-import { HiX } from 'react-icons/hi';
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { HiX } from "react-icons/hi";
 
-import images from '../constants/images';
+import images from "../constants/images";
 
 const { avatarrest } = images;
 
 const sidebarVariants = {
   hidden: {
     opacity: 0,
-    y: '100%',
+    y: "100%",
   },
   open: {
     opacity: 1,
     y: 0,
     transition: {
-      type: 'tween',
+      type: "tween",
       duration: 0.5,
       damping: 10,
-      when: 'beforeChildren',
+      when: "beforeChildren",
       staggerChildren: 0.1,
     },
   },
   closed: {
     opacity: 0,
-    y: '100%',
+    y: "100%",
     transition: {
-      type: 'tween',
+      type: "tween",
       duration: 1.2,
       damping: 10,
       staggerChildren: 0.1,
@@ -34,7 +34,7 @@ const sidebarVariants = {
   },
   exit: {
     opacity: 0,
-    y: '100%',
+    y: "100%",
   },
 };
 
@@ -57,30 +57,48 @@ function Sidebar({ setToggle, toggle }) {
       <motion.div
         className="container rounded-tl-3xl z-20 top-0 bottom-0 right-0 p-4 w-[50%] h-screen flex flex-col
         justify-end items-end bg-cover bg-no-repeat bg-gradient-to-br from-white to-primary dark:bg-gradient-to-tl dark:from-primary dark:to-black fixed "
-        animate={toggle ? 'open' : 'closed'}
+        animate={toggle ? "open" : "closed"}
         initial="hidden"
         exit="exit"
         variants={sidebarVariants}
       >
-        <HiX onClick={() => setToggle(false)} className="w-[35px] h-[35px] text-primary" />
-        <ul
-          className="links h-screen w-full list-none m-0 p-0 flex flex-col justify-start items-start "
-        >
-          <img src={avatarrest} alt="meanimated" className="flex w-11 h-11 mx-auto object-cover mb-4 max-w-[70px] max-h-[70px] shadow-primary dark:shadow-white shadow-sm rounded-full" />
-          {['HOME', 'ABOUT', 'PROJECTS', 'CONTACT',
-            <a href="https://drive.google.com/file/d/1Ao2pGWNp7wgU9uFilX5GqzV5AJ85zBBd/view?usp=sharing" download="resume.pdf" target="_blank" rel="noreferrer">RESUME</a>].map((item) => (
-              <motion.li
-                variants={listVariant}
-                key={item}
-                className="flex flex-col m-4 cursor-pointer border border-transparent p-2 rounded-lg bg-white backdrop-blur-lg bg-opacity-20 dark:backdrop-blur-sm dark:bg-opacity-5"
+        <HiX
+          onClick={() => setToggle(false)}
+          className="w-[35px] h-[35px] text-primary"
+        />
+        <ul className="links h-screen w-full list-none m-0 p-0 flex flex-col justify-start items-start ">
+          <img
+            src={avatarrest}
+            alt="meanimated"
+            className="flex w-11 h-11 mx-auto object-cover mb-4 max-w-[70px] max-h-[70px] shadow-primary dark:shadow-white shadow-sm rounded-full"
+          />
+          {[
+            "HOME",
+            "ABOUT",
+            "PROJECTS",
+            "CONTACT",
+            <a
+              href="https://drive.google.com/file/d/1GEyh_LKR5QQJ6yAoAjSsrVpOsRyyZB1r/view?usp=sharing"
+              download="resume.pdf"
+              target="_blank"
+              rel="noreferrer"
+            >
+              RESUME
+            </a>,
+          ].map((item) => (
+            <motion.li
+              variants={listVariant}
+              key={item}
+              className="flex flex-col m-4 cursor-pointer border border-transparent p-2 rounded-lg bg-white backdrop-blur-lg bg-opacity-20 dark:backdrop-blur-sm dark:bg-opacity-5"
+            >
+              <a
+                href={`#${item}`}
+                onClick={() => setToggle(false)}
+                className="flex flex-col font-medium text-sm no-underline hover:animate-pulse transition-all duration-500 ease-in-out"
               >
-                <a
-                  href={`#${item}`}
-                  onClick={() => setToggle(false)}
-                  className="flex flex-col font-medium text-sm no-underline hover:animate-pulse transition-all duration-500 ease-in-out"
-                >{item}
-                </a>
-              </motion.li>
+                {item}
+              </a>
+            </motion.li>
           ))}
         </ul>
       </motion.div>
